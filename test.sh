eddef9e32b63eac21ecca6b5690ea23097b20167
Alexandre <a@alex00.com>
2018-08-30 23:12:53 +0200

    time in tests

--- test.sh
+++ test.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+TIMEFORMAT="   %Rs"
+
 for p in "$*"
 do
 	for param in $p
 	do
 		cd $param 2>/dev/null || continue
 		sh "compile.sh" || (cd .. && continue)
+		
+		echo ""
 
-		./program ../hello_world.bf > hello_world.out
+		time ./program ../hello_world.bf > hello_world.out
 		diff ../hello_world.ref hello_world.out > /dev/null && echo "OK $param HELLO_WORLD" || echo "ERROR $param HELLO_WORLD ERROR"
 
-		./program ../hello_nested_world.bf > hello_nested_world.out
+		time ./program ../hello_nested_world.bf > hello_nested_world.out
 		diff ../hello_nested_world.ref hello_nested_world.out > /dev/null && echo "OK $param HELLO_NESTED_WORLD" || echo "ERROR $param HELLO_NESTED_WORLD"
 
-		echo "187438238" | ./program ../factor.bf > factor.out
+		time echo "187438238" | ./program ../factor.bf > factor.out
 		diff ../factor.ref factor.out > /dev/null && echo "OK $param FACTOR" || echo "ERROR $param FACTOR"
 		cd ..
 	done
